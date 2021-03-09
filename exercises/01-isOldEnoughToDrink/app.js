473ba152f11d7cbda071c78d1c1bf3fdb0038e30
SilMontes <Silmontes2001@gmail.com>
2021-03-09 16:33:16 +0000

    avance

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,10 @@
-function isOldEnoughToDrink(){
+function isOldEnoughToDrink(number){
     // your code here	
+    if(number>=21){
+        return true;
+    }else{
+        return false
+    }
 }
+var age=isOldEnoughToDrink(18);
+console.log(age);
